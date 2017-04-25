594fed0e9590d49dbe3e8954dcef8edce31bffdb
Maxim <maxim6394@gmail.com>
2017-04-25 13:45:55 +0200

    dummy image und imageeditorpanel hinzugefuegt

--- ImageEditorFrame.java
+++ ImageEditorFrame.java
@@ -5,11 +5,26 @@ import java.awt.image.*;
 import java.io.*;
 import javax.imageio.*;
 
-public class ImageEditorFrame extends JFrame{ 
-	public ImageEditorFrame(){
-		setDefaultCloseOperation(EXIT_ON_CLOSE); 	
+public class ImageEditorFrame extends JFrame {
+	
+	ImageEditorPanel panel;
+	
+	public ImageEditorFrame() {
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(400, 300);
 		setVisible(true);
 		setTitle("asdf");
-	}	 
+		panel = new ImageEditorPanel();
+		add(panel);
+		setDummyImage();
+	}
+
+	private void setDummyImage() {
+		BufferedImage bufferedImage = new BufferedImage(400, 300,
+				BufferedImage.TYPE_INT_RGB);
+		Graphics g = bufferedImage.getGraphics();
+		g.setColor(Color.YELLOW);
+		g.fillOval(10, 10, 380, 280);
+		panel.setImage(bufferedImage);
+	}
 }
