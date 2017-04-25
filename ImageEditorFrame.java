d800dd63e61094b906a92006a0c1ab6e60d26011
Philipp Steinke <phil.steinke@gmx.de>
2017-04-25 13:17:08 +0200

    added menu

--- ImageEditorFrame.java
+++ ImageEditorFrame.java
@@ -5,10 +5,31 @@ import java.awt.image.*;
 import java.io.*;
 import javax.imageio.*;
 
-public class ImageEditorFrame extends JFrame{ 
-public ImageEditorFrame(){
-	setDefaultCloseOperation(EXIT_ON_CLOSE); 	
-	setSize(400, 300);
-	setVisible(true);
-} 
+public class ImageEditorFrame extends JFrame {
+	public ImageEditorFrame() {
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(400, 300);
+		setVisible(true);
+	}
+
+	private void createMenuBar() {
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		JMenu menuFile = new JMenu("File");
+		menuBar.add(menuFile);
+		JMenuItem menuItemOpen = new JMenuItem("Open");
+		menuFile.add(menuItemOpen);
+		menuItemOpen.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				onOpen();
+			}
+
+		});
+	}
+
+	private void onOpen() {
+		JOptionPane.showMessageDialog(this, "Open Selected");
+	}
+
 }
